1dbfacd1ea0f6636eae94148e826ccb9ea833914
Artano <ArtanoHimself@gmail.com>
2024-09-09 19:19:46 +0300

    homework 15

---
+++ hw15/hw15/ViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    lazy var someButton: UIButton = .init()
+    lazy var infoLabel = UILabel()
+    lazy var customAlert: CustomView = CustomView.init(frame: .init(x: 20, y: 300, width: 350, height: 200))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        
+    }
+    
+    func setupUI() {
+        buttonConfig()
+        labelSetup()
+        view.addSubview(customAlert)
+        customAlert.isHidden = true
+        customAlert.delegate = self
+        
+    }
+    
+    func buttonConfig() {
+        view.addSubview(someButton)
+        setButtonConstraints()
+        buttonLook()
+        someButton.addTarget(self, action: #selector(showAndHide), for: .touchUpInside)
+        
+    }
+    
+    func setButtonConstraints() {
+        someButton.translatesAutoresizingMaskIntoConstraints                                     = false
+        someButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive                = true
+        someButton.centerYAnchor.constraint(equalTo: view.centerYAnchor ,constant: 300).isActive = true
+        someButton.heightAnchor.constraint(equalToConstant: 40).isActive                         = true
+        someButton.widthAnchor.constraint(equalToConstant: 100).isActive                         = true
+    }
+    
+    func buttonLook() {
+        someButton.setTitle("Press me", for: .normal)
+        someButton.setTitleColor(.black, for: .normal)
+        someButton.setTitleColor(.gray, for: .highlighted)
+        someButton.backgroundColor = .lightGray
+        someButton.layer.cornerRadius = 20
+    }
+    
+    func labelSetup() {
+        view.addSubview(infoLabel)
+        infoLabel.translatesAutoresizingMaskIntoConstraints                      = false
+        infoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        infoLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        infoLabel.text = "Thanks, buddy, appreciate that"
+        infoLabel.textColor = .systemBackground
+        infoLabel.isHidden = true
+        
+    }
+        
+    @objc func showAndHide() {
+        customAlert.isHidden.toggle()
+        infoLabel.isHidden = true
+    }
+}
+
+extension ViewController: ButtonTapDelegate {
+    func labelVisibility(isOff: Bool) {
+        infoLabel.isHidden = isOff
+    }
+}
+
+
