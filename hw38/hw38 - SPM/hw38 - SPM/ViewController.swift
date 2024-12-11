59bdffbe591376ec940b587eb9aa0caef265c9a5
ArtanoHimself <artanohimself@gmail.com>
2024-12-12 00:22:56 +0300

    SPM, CocoaPods (#28)

---
+++ hw38/hw38 - SPM/hw38 - SPM/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  hw38 - SPM
+//
+//  Created by Artano on 6.12.24.
+//
+
+import UIKit
+import Alamofire
+import FirebaseCrashlyticsSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw38/hw38CocoaPods/hw38CocoaPods/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  hw38CocoaPods
+//
+//  Created by Artano on 11.12.24.
+//
+
+import UIKit
+import Crashlytics
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
